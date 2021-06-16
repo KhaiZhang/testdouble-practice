def6718dfb0e2c157be0123db4fd0330639743ac
khai <KhaiZhang@163.com>
2021-06-17 00:08:27 +0800

    [Khai][add test][should_return_same_trade_with_trade_find_by_trade_repository_when_trading_service_find_trade

--- src/test/java/com/tw/comprehensive/TradingServiceTest.java
+++ src/test/java/com/tw/comprehensive/TradingServiceTest.java
@@ -1,7 +1,10 @@
 package com.tw.comprehensive;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
 
 class TradingServiceTest {
 
@@ -21,4 +24,19 @@ class TradingServiceTest {
         Mockito.verify(spyAuditService).logNewTrade(trade);
     }
 
+    @Test
+    void should_return_same_trade_with_trade_find_by_trade_repository_when_trading_service_find_trade() {
+        //given
+        TradeRepository tradeRepository = mock(TradeRepository.class);
+        AuditService auditService = new AuditService();
+        TradingService tradingService = new TradingService(tradeRepository, auditService);
+        Trade trade = new Trade("test", "test");
+        Mockito.when(tradeRepository.findById(1L)).thenReturn(trade);
+
+        //when
+        Trade findTrade = tradingService.findTrade(1L);
+
+        //then
+        Assertions.assertEquals(trade, findTrade);
+    }
 }
