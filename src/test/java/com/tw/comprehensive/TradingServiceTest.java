befc8cc6e1d6dfd9b98c172cf7a3b7d3bba8d887
khai <KhaiZhang@163.com>
2021-06-17 00:01:50 +0800

    [Khai][add test][should_log_new_trade_when_create_trade_given_new_trade]

--- src/test/java/com/tw/comprehensive/TradingServiceTest.java
+++ src/test/java/com/tw/comprehensive/TradingServiceTest.java
@@ -1,7 +1,24 @@
 package com.tw.comprehensive;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class TradingServiceTest {
+
+    @Test
+    void should_log_new_trade_when_create_trade_given_new_trade() {
+        //given
+        TradeRepository tradeRepository = new TradeRepository();
+        AuditService auditService = new AuditService();
+        AuditService spyAuditService = Mockito.spy(auditService);
+        TradingService tradingService = new TradingService(tradeRepository, spyAuditService);
+        Trade trade = new Trade("test", "test");
+
+        //when
+        tradingService.createTrade(trade);
+
+        //then
+        Mockito.verify(spyAuditService).logNewTrade(trade);
+    }
 
 }
